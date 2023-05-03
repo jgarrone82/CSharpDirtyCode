c39650d2a54cf25cb5d4e0030e929a06a0f34456
Jorge Ariel Garrone <jorge.garrone@gmail.com>
2023-05-03 10:13:35 +0200

    Add Const for cashier program

--- CajeroAutomatico/Program.cs
+++ CajeroAutomatico/Program.cs
@@ -5,60 +5,47 @@ namespace CajeroAutomatico
     class Program
     {
         static void Main(string[] args)
-        {
-            int a = 0;
-            Program program = new Program();
-
-            int deposito;
-            int saldo = 5000;
-            int retiro; // variable para guardar retiro de dinero
+        {       
+            string buildTitle(string title) 
+            {
+                return @$"
+                
+______________________________________________________________________
+                
+            {title}  
+______________________________________________________________________
+                
+                ";
+            }            
+         
+            int saldo = ProgramConsts.InitialAmount;
+            int operationSelected;
 
             do
             {
-                program.menu();
-                int op = Convert.ToInt32(Console.ReadLine());
-                switch (op)
+                Console.WriteLine(ProgramConsts.Menu);
+                int operation = Convert.ToInt32(Console.ReadLine());
+                switch (operation)
                 {
                     case 1:
                         {
-                            Console.WriteLine("");
-                            Console.WriteLine("______________________________________________________________________");
-                            Console.WriteLine("");
-                            Console.WriteLine("             INGRESE EL MONTO QUE DESEA DEPOSITAR  ");
-                            Console.WriteLine("______________________________________________________________________");
-                            Console.WriteLine("");
-                            deposito = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine("");
-                            saldo = saldo + deposito;
+                            Console.WriteLine(buildTitle("INGRESE EL MONTO QUE DESEA DEPOSITAR"));
+                            saldo = saldo + Convert.ToInt32(Console.ReadLine());;
                             Console.WriteLine("         TU MONTO DE DINERO SE HA GUARDADO CORRECTAMENTE");
-
                         }
                         break;
 
                     case 2:
                         {
-                            Console.WriteLine("");
-                            Console.WriteLine("_______________________________________________________________________");
-                            Console.WriteLine("");
-                            Console.WriteLine("                     CONSULTA DE SALDO");
-                            Console.WriteLine("_______________________________________________________________________");
-                            Console.WriteLine("");
-
+                            Console.WriteLine(buildTitle("CONSULTA DE SALDO"));
                             Console.WriteLine("        TU SALDO ACTUAL ES {0} ", saldo);
                             Console.WriteLine("");
                         }
                         break;
                     case 3:
                         {
-
-                            Console.WriteLine("");
-                            Console.WriteLine("________________________________________________________________________");
-                            Console.WriteLine("");
-                            Console.WriteLine("                     INGRESE EL MONTO QUE DESEE RETIRAR ");
-                            Console.WriteLine("________________________________________________________________________");
-                            Console.WriteLine("");
-                            Console.WriteLine("");
-                            retiro = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(buildTitle("INGRESE EL MONTO QUE DESEE RETIRAR"));
+                            int retiro = Convert.ToInt32(Console.ReadLine());
                             saldo = saldo - retiro;
                             Console.WriteLine("");
                             Console.WriteLine("     TU RETIRO DE {0} SE REALIZO CORRECTAMENTE ", retiro);
@@ -75,30 +62,10 @@ namespace CajeroAutomatico
                         break;
                 }
                 Console.ReadKey();
-                a = op;
+                operationSelected = operation;
 
-            } while (a != 4);
+            } while (operationSelected != 4);
 
-        }
-
-
-        /// <summary>
-        /// Pinta el menu de la aplicacion de consola
-        /// </summary>
-        public void menu()
-        {
-            Console.WriteLine("______________________________________________________________________");
-            Console.WriteLine("                                                                  ");
-            Console.WriteLine("                   BIENVENIDO AL CAJERO AUTOMATICO");
-            Console.WriteLine("______________________________________________________________________");
-            Console.WriteLine("");
-            Console.WriteLine("      1.- DESEA DEPOSITAR");
-            Console.WriteLine("      2.- DESEA CONSULTAR SU SALDO");
-            Console.WriteLine("      3.- DESEA RETIRAR");
-            Console.WriteLine("      4.- DESEA SALIR");
-            Console.WriteLine("");
-            Console.WriteLine("______________________________________________________________________");
-            Console.WriteLine("");
         }
     }
 }
---
+++ CajeroAutomatico/ProgramConsts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroAutomatico
+{
+    public static class ProgramConsts
+    {
+        public const int InitialAmount = 5000;
+
+        public static string Menu = buildMenu();
+
+        /// <summary>
+        /// Pinta el menu de la aplicacion de consola
+        /// </summary>
+        private static string buildMenu()
+        {
+            StringBuilder menuStringBuilder = new StringBuilder();
+
+            menuStringBuilder.AppendLine("______________________________________________________________________");
+            menuStringBuilder.AppendLine("                                                                  ");
+            menuStringBuilder.AppendLine("                   BIENVENIDO AL CAJERO AUTOMATICO");
+            menuStringBuilder.AppendLine("______________________________________________________________________");
+            menuStringBuilder.AppendLine("");
+            menuStringBuilder.AppendLine("      1.- DESEA DEPOSITAR");
+            menuStringBuilder.AppendLine("      2.- DESEA CONSULTAR SU SALDO");
+            menuStringBuilder.AppendLine("      3.- DESEA RETIRAR");
+            menuStringBuilder.AppendLine("      4.- DESEA SALIR");
+            menuStringBuilder.AppendLine("");
+            menuStringBuilder.AppendLine("______________________________________________________________________");
+            menuStringBuilder.AppendLine("");
+
+            return menuStringBuilder.ToString();
+        }            
+            
+    }
+}
